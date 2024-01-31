5984f9e9976497bb9ee3c30cdd41965443c9a3b6
giuseppepao <g.paolella@asem.it>
2024-01-31 16:06:23 +0100

    jjjhh

---
+++ ProjectFiles/NetSolution/ApplicationNameLogic.cs
@@ -0,0 +1,22 @@
+#region Using directives
+using FTOptix.HMIProject;
+using FTOptix.NetLogic;
+using FTOptix.UI;
+using FTOptix.SerialPort;
+using FTOptix.S7TiaProfinet;
+using FTOptix.CommunicationDriver;
+using FTOptix.S7TCP;
+#endregion
+
+public class ApplicationNameLogic : BaseNetLogic
+{
+    public override void Start()
+    {
+        Label label = Owner as Label;
+        label.Text = Project.Current.BrowseName;
+    }
+
+    public override void Stop()
+    {
+    }
+}
